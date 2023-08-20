7313cf0ac5ad37a0de0b7d29ae0cc6ac33f3cf1c
GENDHUT <Kecikcemrincingan15@gmail.com>
2023-08-20 17:21:20 +0700

    sedikit merubah

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-<<<<<<< HEAD
-use Illuminate\Support\Facades\auth;
+use illuminate\Support\Facades\auth;
 
 class LoginController extends Controller
 {
@@ -23,39 +22,9 @@ class LoginController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/');
-=======
-use illuminate\Support\Facades\auth;
-
-class LoginController extends Controller
-{
-    public function index(){
-        return view('login');
-    }
-
-    public function authentic(Request $request){
-        $credentials = $request->validate([
-            'email'=> 'required|email',
-            'password'=> 'required'
-        ]);
-
-        if(Auth::attempt($credentials)){
-            $request->session()->regenerate();
             return redirect()->intended('/home');
->>>>>>> 5579e37ecd7cf7acb03ed0853cb32ba47c757e87
         }
 
         return back()->with('LoginError', 'Login gagal');
     }
-<<<<<<< HEAD
-
-    public function logout()
-    {
-        Auth::logout();
-        request()->session()->invalidate();
-        request()->session()->regenerateToken();
-        return redirect('/');
-    }
-=======
->>>>>>> 5579e37ecd7cf7acb03ed0853cb32ba47c757e87
 }
